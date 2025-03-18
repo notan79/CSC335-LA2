afcec535346d581b172fe1361eaf6c06c1c06932
cameron liu <cameronliu@camerons-MacBook-Air.local>
2025-03-17 18:15:06 -0700

    fixed hashing and salting

--- src/model/library/User.java
+++ src/model/library/User.java
@@ -8,26 +8,37 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 import java.lang.StringBuilder;
+import java.util.Scanner;
+import java.io.FileNotFoundException; 
 
 
 
 public class User extends LibraryModel {
-	private String username; 
-	private String password;
+	
+//	public static void main(String[] args) {
+//	
+//		User user = new User("uname", "password");
+//		User user2 = new User("u2", "pass");
+//		
+//		System.out.println(user.validateLogin("uname", "password"));
+//		System.out.println(user2.validateLogin("u2", "password"));
+//	}
+	
+	
+	private final String username; 
+	private final String password;
+	private final static int saltLength = 64;
 	
 	public User(String username, String password) {
 		this.username = username; 
-		this.password = password;
+		this.password = salt(encrypt(password));
 		writeFile();
 	}
 	
 	private void writeFile() {
 		try {
-			FileWriter fileWriter = new FileWriter(new File("data/login"), true);
-			String salted = salt(password);
-			String encrypted = encrypt(salted);
-			
-			fileWriter.write(username + " " + encrypted + "\n");
+			FileWriter fileWriter = new FileWriter(new File("data/login"), true);			
+			fileWriter.write(username + " " + password + "\n");
 			fileWriter.close();
 		} catch (IOException e) {
 			// This can never happen, the file is already created
@@ -39,10 +50,10 @@ public class User extends LibraryModel {
 	private String salt(String password) {
 		Random random = new Random();
 		StringBuilder sb = new StringBuilder(password);
-		for (int i = 0; i < 64; i++) {
-			char randChar = (char)('a' + random.nextInt(93));
+		for (int i = 0; i < User.saltLength; i++) {
+			char randChar = (char)('!' + random.nextInt(93));
 			sb.append(String.valueOf(randChar));
-		}
+		}		
 		return sb.toString();
 	}
 	
@@ -51,9 +62,11 @@ public class User extends LibraryModel {
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
 			
-			BigInteger encrypted = new BigInteger(md.digest(password.getBytes()));
+			byte[] mDigest = md.digest(password.getBytes());
 			
-			temp = encrypted.toString(16);
+			BigInteger bi = new BigInteger(1, mDigest);
+			
+			temp = bi.toString(16);
 			
 		} catch (NoSuchAlgorithmException e) {
 			// This can never happen
@@ -62,5 +75,41 @@ public class User extends LibraryModel {
 		
 		return temp;
 	}
+	
+	
+	// used for 
+	public boolean validateLogin(String username, String password) {
+		// checks if the user has the same user and pass
+		String temp = usernameExist(username);
+		temp = temp.substring(0, temp.length() - User.saltLength);
+		return temp != null && temp.equals(encrypt(password));
+	}
+	
+	private String usernameExist(String username) {
+		// returns the encrypted and salted pass associated with user
+		File file = new File("data/login");
+		try {
+			Scanner scan = new Scanner(file);
+			while (scan.hasNext()) {		
+				String[] lines = scan.nextLine().split(" ");
+				// assumes that there is none of the same username
+				if (lines[0].equals(username)) {
+					return lines[1];
+				}
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			// this can never happen
+			System.exit(1);
+		} 
+		return null;
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 }
